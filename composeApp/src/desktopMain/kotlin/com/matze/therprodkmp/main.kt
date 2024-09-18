257287e2af5ce13f5c344c8d5ed2bf12fc427fcd
rmatze <rmatze@gmail.com>
2024-09-18 15:50:59 -0600

    import app for desktop

--- composeApp/src/desktopMain/kotlin/com/matze/therprodkmp/main.kt
+++ composeApp/src/desktopMain/kotlin/com/matze/therprodkmp/main.kt
@@ -2,6 +2,7 @@ package com.matze.therprodkmp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.matze.App
 
 fun main() = application {
     Window(
